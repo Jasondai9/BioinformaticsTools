ab1d89b213a00ddde2e3004d05d3aee3a875c15d
Jason Dai <jad054@tscc-login3.sdsc.edu>
2019-04-26 12:13:37 -0700

    More robust

--- bam_convert.sh
+++ bam_convert.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-FILES=./*.bam
+USAGE="USAGE: bam_convert.sh [path/to/fastq] [path/to/bam/directory]\n*Path to BAM directory should be a folder containing BAM files"
+
+if [ "$1" != "" ] && ["$2" != ""]; then
+    PATH_TO_FASTQ=$1
+	PATH_TO_BAM=$2
+else
+    printf $USAGE
+fi
+
+FILES=$2/*.bam
 
 for f in $FILES
 do
 	printf "Converting $f...\n"
 	fname=`basename $f .bam`
-	samtools bam2fq -1 ${fname}_1.fastq -2 ${fname}_2.fastq $f
+	echo samtools bam2fq -1 $PATH_TO_FASTQ/${fname}_1.fastq -2 $PATH_TO_FASTQ/${fname}_2.fastq $f
 done
