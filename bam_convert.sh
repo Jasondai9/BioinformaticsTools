456dd742ca3fcc75e380e7393b3c53bb6b3355e6
Jason Dai <jad054@tscc-login3.sdsc.edu>
2019-04-29 13:01:11 -0700

    asks for confirmation

--- bam_convert.sh
+++ bam_convert.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-USAGE="USAGE: bam_convert.sh [path/to/fastq] [path/to/bam/directory]\n*Path to BAM directory should be a folder containing BAM files"
 
-if [ "$1" != "" ] && ["$2" != ""]; then
-    PATH_TO_FASTQ=$1
-	PATH_TO_BAM=$2
+if [ "$1" == "" ] || ["$2" == ""]; then
+    USAGE="USAGE: bam_convert.sh [path/to/fastq] [path/to/bam/directory]\n*Path to BAM directory should be a folder containing BAM files\n"
+    printf "$USAGE"
 else
-    printf $USAGE
+	PATH_TO_FASTQ="$1"
+	PATH_TO_BAM="$2"
+	CONFIRMATION="$3"
+
+	FILES=$2/*.bam
+
+	#No confirmation
+: '
+	echo ${PATH_TO_FASTQ}
+	echo ${PATH_TO_BAM}
+	for f in $FILES
+	do
+		printf "Converting $f...\n"
+		fname=`basename $f .bam`
+
+		samtools bam2fq -1 ${PATH_TO_FASTQ}/${fname}_1.fastq -2 ${PATH_TO_FASTQ}/${fname}_2.fastq $f
+	done
+'
+
+	#Ask for confirmation
+	echo "Path to FASTQ is: ${PATH_TO_FASTQ}"
+	echo "Path to BAM is: ${PATH_TO_BAM}"
+	read -p "Is this correct? (Y/n)" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		for f in $FILES
+		do
+			printf "Converting $f...\n"
+			fname=`basename $f .bam`
+			samtools bam2fq -1 ${PATH_TO_FASTQ}/${fname}_1.fastq -2 ${PATH_TO_FASTQ}/${fname}_2.fastq $f
+		done
+	fi
+
 fi
-
-FILES=$2/*.bam
-
-for f in $FILES
-do
-	printf "Converting $f...\n"
-	fname=`basename $f .bam`
-	samtools bam2fq -1 $PATH_TO_FASTQ/${fname}_1.fastq -2 $PATH_TO_FASTQ/${fname}_2.fastq $f
-done
