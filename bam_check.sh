a5918b7ae74c628618c69c214264ad0168a8f937
Jason Dai <jad054@tscc-login3.sdsc.edu>
2019-05-01 16:08:39 -0700

    checks bam files

---
+++ bam_check.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+FILES=./*flagstat*
+
+
+#if they want all files then add "all" after the command
+if [ "$1" == "all" ]
+then
+	printf "Stats for all BAMS\nSAMPLE\tPASS\tFAIL\n"
+	for f in $FILES
+	do
+		fname=`basename $f _flagstat.txt`
+		PASSED_READS="$(head -1 $f | cut -f1 -d' ')"
+		FAILED_READS="$(head -1 $f | cut -f3 -d' ')"
+		printf "$fname\t$PASSED_READS\t$FAILED_READS\n"
+	done
+
+#if no parameters then just print the important ones
+else
+	printf "BAM files that had less than 1000 passes or nonzero fails:\nSAMPLE\tPASS\tFAIL\n"
+	for f in $FILES
+	do
+		fname=`basename $f _flagstat.txt`
+		PASSED_READS="$(head -1 $f | cut -f1 -d' ')"
+		FAILED_READS="$(head -1 $f | cut -f3 -d' ')"
+
+		#print the stats if less than 1000 passed or greater than 0 fails
+		if (( $PASSED_READS < 1000 )) || (( $FAILED_READS > 0 )); 
+			then
+				printf "$fname\t$PASSED_READS\t$FAILED_READS\n"
+		fi
+	done
+fi
