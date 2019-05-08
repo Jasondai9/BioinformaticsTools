6edaac8f44a32eb85749f95a9cce95d97f1c8628
Jason Dai <jad054@tscc-login3.sdsc.edu>
2019-05-08 16:58:24 -0700

    handles blank files

--- bam_check.sh
+++ bam_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FILES=./*flagstat*
 
+#call from the bam folder containing flagstats
 
 #if they want all files then add "all" after the command
 if [ "$1" == "all" ]
@@ -20,13 +21,16 @@ else
 	for f in $FILES
 	do
 		fname=`basename $f _flagstat.txt`
-		PASSED_READS="$(head -1 $f | cut -f1 -d' ')"
-		FAILED_READS="$(head -1 $f | cut -f3 -d' ')"
-
+		PASSED_READS=$(head -1 $f | cut -f1 -d' ')
+		FAILED_READS=$(head -1 $f | cut -f3 -d' ')
+				
 		#print the stats if less than 1000 passed or greater than 0 fails
-		if (( $PASSED_READS < 1000 )) || (( $FAILED_READS > 0 )); 
+		if [ "$PASSED_READS" != "" ] && [ "$FAILED_READS" != "" ];
+		then
+			if [ "$PASSED_READS" -lt 1000 ] || [ "$FAILED_READS" -gt 0 ]; 
 			then
 				printf "$fname\t$PASSED_READS\t$FAILED_READS\n"
+			fi
 		fi
 	done
 fi
