e1ceabb43e0def75bc5b5740ac2ca5a034a13b56
Frances Keer <fkeer@tscc-login3.sdsc.edu>
2019-05-09 14:15:27 -0700

    now checks size of bams

--- align_check.sh
+++ align_check.sh
@@ -2,8 +2,9 @@
 
 #align_check.sh name_of_submit_folder
 #must be called from bam folder in analyzed sample directory
-
+gig=1000000000
 echo "Incomplete tumor samples:" > missing_files.txt
+echo "Too small tumor final bams:" > small_bams.txt
 for tsample in $(sed 1d ../../submits/$1/*.txt | cut -f2)
 do
 	tcount=$(ls -l $tsample* | wc -l) 
@@ -14,9 +15,20 @@ do
 		echo "$tsample" "is missing files! See missing_files.txt"
 		echo "$tsample" >> missing_files.txt
 	fi
+
+	sizebam=$(ls -l ${tsample}.bam | cut -f 5 -d ' ')
+	sizefinalbam=$(ls -l ${tsample}_final.bam | cut -f 5 -d ' ')
+
+	if [ "$sizefinalbam" -gt "$sizebam" ] && [ "$sizebam" -gt "$gig" ];then
+		echo "$tsample" "final bam is acceptable size"
+	else
+		echo "$tsample" "final bam is not acceptable size! See small_bams.txt"
+		echo "$tsample" >> small_bams.txt
+	fi
 done
 
 echo "Incomplete normal samples:" >> missing_files.txt
+echo "Too small normal final bams:" >> small_bams.txt
 for nsample in $(sed 1d ../../submits/$1/*.txt | cut -f3)
 do
 	ncount=$(ls -l $nsample* | sort | uniq | wc -l) 
@@ -27,4 +39,16 @@ do
 		echo "$nsample" "is missing files! See missing_files.txt"
 		echo "$nsample" >> missing_files.txt
 	fi
+
+	sizebam=$(ls -l ${nsample}.bam | cut -f 5 -d ' ')
+	sizefinalbam=$(ls -l ${nsample}_final.bam | cut -f 5 -d ' ')
+
+	if [ "$sizefinalbam" -gt "$sizebam" ] && [ "$sizebam" -gt "$gig" ];then
+		echo "$nsample" "final bam is acceptable size"
+	else
+		echo "$nsample" "final bam is not acceptable size! See small_bams.txt"
+		echo "$nsample" >> small_bams.txt
+	fi
 done
+
+
