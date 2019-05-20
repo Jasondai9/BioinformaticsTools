8ecf9e7288bc13890a53fc8135e00dbbd608430e
Jason Dai <jad054@tscc-login3.sdsc.edu>
2019-05-20 12:55:52 -0700

    chmod at end

--- vcf_filter.sh
+++ vcf_filter.sh
@@ -7,16 +7,21 @@ else
 	INPUT_VCF="$1"
 	FILTER_VCF="$2"
 
-	FILTERED_BASE=`basename $FILTER_VCF .vcf`
+	FILTERED_BASE=`basename $FILTER_VCF .vcf.gz`
 	INPUT_BASE=`basename $INPUT_VCF .vcf`
 	CLEANED_FILTER=${FILTERED_BASE}_cleaned.vcf
+	CLEANED_INPUT=${INPUT_BASE}_cleaned.vcf
 	FILTERED_INPUT=${INPUT_BASE}_filtered.vcf
 
-	grep -v "#" ${FILTER_VCF} | cut -f1,2 > ${CLEANED_FILTER}
+	#cleans by removing comments and chr
+	zgrep -v "#" ${FILTER_VCF} | sed 's/chr//g' > ${CLEANED_FILTER}
+	grep -v "#" ${INPUT_VCF} | sed 's/chr//g' > ${CLEANED_INPUT}
 
+	#removes rows where
+	awk 'NR==FNR{field[$1 $2 $4 $5]++;next};!field[$1 $2 $4 $5]' ${CLEANED_FILTER} ${CLEANED_INPUT} > ${FILTERED_INPUT}
 
-	awk 'NR==FNR{field[$1 $2]++;next};!field[$1 $2]' ${CLEANED_FILTER} ${INPUT_VCF} > ${FILTERED_INPUT}
-
-	rm ${CLEANED_FILTER}
+	chmod 775 ${CLEANED_FILTER}
+	chmod 775 ${CLEANED_INPUT}
+	chmod 775 ${FILTERED_INPUT}
 fi
 
