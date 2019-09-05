f9981211f5d4254a48f52455a20df28c701ce809
Jason Dai <jad054@tscc-login3.sdsc.edu>
2019-09-05 14:19:13 -0700

    bug fixes

--- samtools_fastq.sh
+++ samtools_fastq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "" ] || [ "$2" == "" ]; then
-    USAGE="\nUSAGE: samtools_fastq.sh \\\n[path/to/fastq] \\\n[path/to/bam/directory] \\\n[OPTIONAL: text file containing path to individual bams] \n
+    USAGE="\nUSAGE:\tsamtools_fastq.sh \\ \n\t[path/to/fastq] \\ \n\t[path/to/bam/directory] \\ \n\t[OPTIONAL: text file containing path to individual bams] \n
 	Path to BAM directory should be a folder containing BAM files 
 	Can optionally pass in a txt file containing path to the files \n\n"
     printf "$USAGE"
@@ -9,20 +9,22 @@ else
 	PATH_TO_FASTQ="$1"
 	PATH_TO_BAM="$2"
 
-	FILES=$2/*.bam
+	FILES=$PATH_TO_BAM/*.bam
 	if [ "$3" != "" ]; then
-		printf "Using BAMs from $3"
-		#the text file
+		printf "Using BAMs from $3\n\n"
 		FILES=$(cat $3)
+		cd $PATH_TO_BAM
 	fi
+
+	mkdir -p $PATH_TO_FASTQ
 
 
 	for f in $FILES
 	do
-		printf "Converting $f. START: $(date)\n"
+		printf "Converting $f START: $(date)\n"
 		fname=`basename $f .bam`
 		
-		samtools sort -n -o ${PATH_TO_FASTQ}/${fname}_sorted.bam -m $(free -h| grep Mem | awk '{print $4}') -@$(nproc) $f
+		samtools sort -n -o ${PATH_TO_FASTQ}/${fname}_sorted.bam -m 6G -@$(nproc) $f
 		samtools fastq -1 ${PATH_TO_FASTQ}/${fname}_1.fastq.gz -2 ${PATH_TO_FASTQ}/${fname}_2.fastq.gz -0 ${PATH_TO_FASTQ}/${fname}_ambiguous.fastq.gz ${PATH_TO_FASTQ}/${fname}_sorted.bam
 
 		rm ${PATH_TO_FASTQ}/${fname}_sorted.bam
@@ -31,4 +33,5 @@ else
 
 	done
 	chmod 775 ${PATH_TO_FASTQ}/*.fastq.gz
+
 fi
