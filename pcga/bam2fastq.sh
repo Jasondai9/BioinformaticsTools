5ead897ab557ddfc4c378cc7cd05a29b39d5d349
Jason Dai <jad054@tscc-login3.sdsc.edu>
2021-07-05 13:59:16 -0700

    invert

--- pcga/bam2fastq.sh
+++ pcga/bam2fastq.sh
@@ -12,14 +12,15 @@ PATH_TO_FASTQ_DIR="$1"
 PATH_TO_BAM="$2"
 TYPE="$3"
 
-if [ -f "$3" ]; then
+if [ ! -f "$3" ]; then
 	TYPE=bam
 fi
 
 mkdir -p $PATH_TO_FASTQ_DIR
 
-fname=`basename $PATH_TO_BAM .${TYPE}`
-
+fname=$(basename $PATH_TO_BAM .${TYPE})
+echo $TYPE $fname
+exit
 samtools sort -n -o ${PATH_TO_FASTQ_DIR}/${fname}_sorted.${TYPE} -@$(nproc) $PATH_TO_BAM
 samtools fastq -1 ${PATH_TO_FASTQ_DIR}/${fname}_1.fastq.gz -2 ${PATH_TO_FASTQ_DIR}/${fname}_2.fastq.gz -s ${PATH_TO_FASTQ_DIR}/${fname}_singleton.fastq.gz -0 ${PATH_TO_FASTQ_DIR}/${fname}_ambiguous.fastq.gz ${PATH_TO_FASTQ_DIR}/${fname}_sorted.${TYPE}
 
